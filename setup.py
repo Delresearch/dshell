9ba6e63a07cf5cf6841702446e3a0cfa9912361b
James DellaMorte <john@delresearch.com>
2018-04-30 14:33:33 -0400

    Added script

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 
 setup(name='dshell', version='1.02', description='Dolphin Shell for Acoustic Modem',
 	url='http://github.com/delresearch/dshell', author='delResearch LLC.', author_email='info@delresearch.com',
-	license='MIT', packages=['dshell'], install_requires=['cmd2', 'Queue'], zip_safe=False)
+	license='MIT', packages=['dshell'], install_requires=['cmd2'],
+	scripts=['bin/dshell'], zip_safe=False)
 
